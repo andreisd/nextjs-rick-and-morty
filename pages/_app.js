c904e5b87ba9b3e4a165a3586825f871f3730940
Andrei Savu <savuandreid@gmail.com>
2022-03-28 02:07:40 +0300

    AnimatePresence exitBeforeEnter

--- pages/_app.js
+++ pages/_app.js
@@ -3,26 +3,26 @@ import "../styles/globals.scss";
 import { motion, AnimatePresence } from "framer-motion";
 export default function MyApp({ Component, pageProps, router }) {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <motion.div
         key={router.route}
-        initial='pageInitial'
-        animate='pageAnimate'
-        exit='pageExit'
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
         variants={{
           pageInitial: {
             opacity: 0,
-            scale: 0,
+            scale: 0.85,
           },
           pageAnimate: {
             opacity: 1,
             scale: 1,
           },
           pageExit: {
-            filter: 'invert()',
+            filter: "invert()",
             opacity: 0,
-            scale: 0,
-          },  
+            scale: 0.85,
+          },
         }}
       >
         <Layout>
